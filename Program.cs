ca15b17fb6c75f3e3d11bbff01f50be30059009d
Wofsauge <jan-.-@t-online.de>
2022-01-25 20:56:28 +0100

    Initial setup of project files

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IsaacSavegameToLua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to close the program");
+            Console.ReadKey();
+        }
+    }
+}
